0c9843936a725866614977f1ce4f2491eadea038
edixon1 <eliotmdixon@gmail.com>
2020-04-24 13:29:06 -0400

    Completed exercise solution script

--- splitGeometry.R
+++ splitGeometry.R
@@ -1,60 +1,62 @@
-#Tahoe National Forest polygon
-forest <- geojson_sf("https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_ForestSystemBoundaries_01/MapServer/0/query?where=FORESTNAME+LIKE+%27%25Tahoe+National%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&having=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson")
+library(sf)
+library(leaflet)
+library(geojsonsf)
+library(dplyr)
+library(urltools)
 
-#Get bounding box of taho national forest polygon and convert to
-tahoeBbox <- st_bbox(forest) %>%
+#Construct query to national forest API
+tahoeBaseURL <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_ForestSystemBoundaries_01/MapServer/0/query?"
+tahoeQuery <- urltools::param_set(tahoeBaseURL, key="where", 
+                          value=urltools::url_encode("FORESTNAME LIKE '%Tahoe National%'")) %>%
+                        param_set(key="outFields", value="*") %>%
+                        param_set(key="f", value="geojson")
+#query national forest API
+tahoe <- geojson_sf(tahoeQuery)
+
+#obtain bounding box for tahoe national and convert to polygon
+tahoeBox <- st_bbox(tahoe) %>%
   st_as_sfc()
+#split bounding box ploygon into 2x2 grid
+tahoeBoxSplit <- st_make_grid(tahoeBox, n=2)
 
-#split bounding box polygon into a 2x2 grid
-bbox_split <- st_make_grid(tahoeBbox,n=2)
-
-epsg <- st_crs(forest)$epsg
-
-query <- urltools::param_set(baseURL,key="inSR", value=epsg) %>%
+#Construct query to invasive species location API
+invasiveBaseURL <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_InvasiveSpecies_01/MapServer/0/query?"
+epsg <- st_crs(tahoeBoxSplit)$epsg
+invasiveQuery <- param_set(invasiveBaseURL,key="inSR", value=epsg) %>%
   param_set(key="spatialRel", value="esriSpatialRelIntersects") %>%
   param_set(key="f", value="geojson") %>%
   param_set(key="outFields", value="*")
 
-#baseURL for  Forest Service invasive species API
-baseURL <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_InvasiveSpecies_01/MapServer/0/query?"
 
-all_invasives <- data.frame()
-
-
-mapview(bbox_split)
-for(polygon in bbox_split){
-  bbox <- st_bbox(polygon) %>%
+#for each quarter of the bounding box, query API and add each returned sf object to invasives list
+invasives <- list()
+for(i in 1:4){
+  #derive and format bounding box of this quarter
+  bbox <- st_bbox(tahoeBoxSplit[i]) %>%
     toString() %>%
     urltools::url_encode()
-  query <- param_set(query,key="geometry", value=bbox)
-  
-  print(NROW(geojson_sf(query)))
-  #all_invasives <- rbind(all_invasives,geojson_sf(query))
+  #add formatted bounding box to query
+  invasiveQuery <- param_set(invasiveQuery, key="geometry", value=bbox)
+  #query API
+  invasive <- geojson_sf(invasiveQuery)
+  #add returned sf object to invasives list
+  invasives[[i]] <- invasive
 }
 
+#vertically combine data returned for each quarter of tahoe bounding box
+invasives <- do.call(rbind, invasives)
 
+#filter observations that fall outside the tahoe national forest
+insideInvasives <- st_join(invasives, tahoe) %>%
+  na.omit()
 
+#Plot four bbox quandrants, with invasives before(red) and after(blue) filtering and tahoe national
+overlayGroups <- c("Tahoe Bounding Quandrants","Tahoe National","Inside Invasives","All invasives")
+leaflet() %>%
+  addProviderTiles("Esri.WorldStreetMap") %>%
+  addPolygons(data=tahoeBoxSplit, weight=2, fill=FALSE, group="Tahoe Bounding Quandrants") %>%
+  addPolygons(data=tahoe, weight=2, color="green", group="Tahoe National") %>%
+  addPolygons(data=insideInvasives,weight=2, color="blue", group="Inside Invasives") %>%
+  addPolygons(data=invasives, weight=1, color="red", group="All invasives") %>%
+  addLayersControl(overlayGroups=overlayGroups, options=layersControlOptions(collapsed=FALSE))
 
-#baseURL for  Forest Service invasive species API
-baseURL <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_InvasiveSpecies_01/MapServer/0/query?"
-#bounding box of tahoe national forest polygon
-bbox <- st_bbox(forest)
-#convert bounding box to character
-bbox <- toString(bbox)
-#encode for use within URL
-bbox <- urltools::url_encode(bbox)
-#EPSG code for coordinate reference system used by tahoe national forest polygon sf object
-epsg <- st_crs(forest)$epsg
-
-#set parameters for query
-query <- urltools::param_set(baseURL,key="geometry", value=bbox) %>%
-  param_set(key="inSR", value=epsg) %>%
-  param_set(key="resultRecordCount", value=500) %>%
-  param_set(key="spatialRel", value="esriSpatialRelIntersects") %>%
-  param_set(key="f", value="geojson") %>%
-  param_set(key="outFields", value="*")
-invasives1 <- geojson_sf(query)
-
-
-#baseURL for  Forest Service invasive species API
-baseURL <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_InvasiveSpecies_01/MapServer/0/query?"
